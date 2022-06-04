883070fc07e285bf74c8a2dd2a1cc745edb61269
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2022-06-05 00:07:30 +1200

    Added the add command.

--- server.py
+++ server.py
@@ -17,21 +17,38 @@ def parser_list_servers(args):
     serverFunctions.list_servers(args.path, args.verbose)
 
 
+def parser_add_server(args):
+    """ Calls add_server() from the parser.
+
+    See serverFunctions.add_server for more details.
+    """
+    serverFunctions.add_server(args.path)
+    
+
+
 ## Main program ##
 if __name__ == "__main__":
+    server_path = os.path.join(os.path.expanduser('~'), ".local/var/servers")
+
     parser = argparse.ArgumentParser(prog="server", description=
             "Handles remote server operations.")
     subparsers = parser.add_subparsers(title="available subcommands")
     
     # List subcommand parser
-    list_parser = subparsers.add_parser('list', help=
-            "List available servers")
+    list_parser = subparsers.add_parser('list', help="List available servers")
     list_parser.add_argument("-v", "--verbose", action="store_true", help=
             "prints extra information")
-    list_parser.add_argument("--path", type=str, default=
-        os.path.join(os.path.expanduser('~'), ".local/var/servers", help=
-            "path to a server list file"))
+    list_parser.add_argument("--path", type=str, default=server_path, help=
+            "path to a server list file")
     list_parser.set_defaults(func=parser_list_servers)
+
+    # Add subcommand parser
+    add_parser = subparsers.add_parser('add', help=
+            "Add a server to the list of available servers")
+    add_parser.add_argument("--path", type=str, default=server_path, help=
+            "path to a server list file")
+    add_parser.set_defaults(func=parser_add_server) 
+
 
     args = parser.parse_args() 
     args.func(args)
